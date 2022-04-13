571a8c0096ce468d0ec28961bd1ccf8f85e0d704
Nabeel Yousaf Pasha <nabeelyousafpasha@gmail.com>
2022-04-14 01:49:14 +0500

    dashboard userguide created and patient link added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,10 +18,32 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Http\RedirectResponse
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
      */
     public function index()
     {
-        return redirect()->route('dashboard.index');
+        return $this->renderView('dashboard.index');
+    }
+
+    /**
+     * @param mixed $view
+     * @param array $withParams
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    protected function renderView($view, array $withParams = [])
+    {
+        $params = [
+            'page' => 'Home',
+            'resource' => '',
+            'translationFromKey' => '',
+            'crud' => [
+                'CREATE_PATIENT' => [
+                    'route' => route('dashboard.patients.create'),
+                    'can' => ! auth()->user()->cannot('create_patient'),
+                ],
+            ],
+        ];
+        return parent::renderView($view, array_merge($withParams, $params));
     }
 }
